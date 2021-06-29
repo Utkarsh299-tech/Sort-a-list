0868b01456696914c75124553adc1ced7d189fac
Utkarsh299-tech <singhutkarsh2799@gmail.com>
2021-06-29 20:05:03 +0530

    Added main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+# list of strings
+from collections import OrderedDict
+strings = ['Hackcation', 'New hack, who this?', 'HackItShipIt',
+           'SigmaHacks 2.0', 'HobbyHacks', 'Garuda Hacks', 'HackMTY', 'MHacks']
+# sorting the list in ascending order
+strings.sort()
+# printing the sorted list
+print(strings)
